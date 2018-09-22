911e2d3ce901b04d7d89e624f595f4b587310041
philanc <philanc@gmail.com>
2018-09-22 15:19:28 -0400

    split junk from he

---
+++ prel_list.lua
@@ -0,0 +1,26 @@
+
+he = require'he'
+
+local function keys(t)
+	local st = {}
+	for k, v in pairs(t) do table.insert(st, k) end
+	return st
+end
+
+local function sortedkeys(t)
+	st = keys(t)
+	table.sort(st)
+	return st
+end
+
+--~ print("\nslua - a static build of Lua 5.3 with some preloaded libraries:")
+print("")
+for i, lib in ipairs(keys(package.preload)) do
+	kl = sortedkeys(require(lib))
+	table.sort(kl)
+	print(string.format("%s --- (%d) %s\n", lib, #kl, table.concat(kl, ", ")))
+--~ 	print(string.format("%s\n", table.concat(kl, ", ")))
+--~  	print("...", table.concat(keys(require(lib)), ", "))
+end
+
+
