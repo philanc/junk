39bc224e2793861ca18c3993233b41ab30b388e6
philanc <philanc@gmail.com>
2019-11-04 00:54:22 -0500

    cleanup test

--- sshkeys/test.lua
+++ sshkeys/test.lua
@@ -1,26 +1,28 @@
+--[[              sshkeys quick test
+
+generate a key with:   
+
+	ssh-keygen -t ed25519 -f aaa
+
+it should create two files in current directory ('aaa' the private key, 
+and 'aaa.pub' the public key)
+
+]]
 
 ------------------------------------------------------------------------
 local he = require "he"
 local b64 = require "plc.base64"
-
-------------------------------------------------------------------------
-
 local sshkeys = require "opensshkeys"
 
-------------------------------------------------------------------------
-
-
--- quick test
-
--- generate a key with:   ssh-keygen -t ed25519 -f aaa
--- it should create two files in current directory ('aaa' the private key, 
--- and 'aaa.pub' the public key)
+local keyname = "aaa"
+local pubkeyfn = keyname .. ".pub"
+local privkeyfn = keyname
 
 local sk, pk, pk2, msg, comment, comment2
 
 -- parse the content of the private key file 
 
-local priv = he.fget("aaa")
+local priv = he.fget(privkeyfn)
 
 local sk, pk, comment = sshkeys.parse_private_key(priv)
 print("private key comment:", comment)
@@ -40,16 +42,15 @@ assert(pk2==pk)
 
 -- parse the content of the public key file 
 
-local pub = he.fget("aaa.pub")
+local pub = he.fget(pubkeyfn)
 pk2, comment2 = sshkeys.parse_public_key(pub)
 if not pk2 then --error
 	msg = comment2
 	print("ERROR:", msg)
 	return
 end
-print("public key comment:", comment2)
-
 assert(pk2 == pk)
+assert(comment2 == comment)
 
 
 
