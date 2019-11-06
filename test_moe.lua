6e58f3c5e5109e02c53f5371162569a77d2061db
philanc <philanc@gmail.com>
2019-11-06 01:16:37 -0500

    cleanup test files

--- test_moe.lua
+++ test_moe.lua
@@ -15,26 +15,30 @@ c = moe.encrypt(k, p, true)
 --~ print("#c, c:", #c, c)
 assert(moe.decrypt(k, c, true) == p)
 --
+-- file names
+local fbase = "/tmp/" .. os.getenv("USER") .. "-moetest"
+local fnp = fbase .. ".p"
+local fnc = fbase .. ".c"
+local fnp2 = fbase .. ".p2"
+--
 x=1200000
 p = ("a"):rep(x)
-he.fput("moedat.p", p)
-local fhi = io.open("moedat.p")
-local fho =  io.open("moedat.c", "w")
+he.fput(fnp, p)
+local fhi = io.open(fnp)
+local fho =  io.open(fnc, "w")
 y, msg = moe.fhencrypt(k, fhi, fho)
---~ print("moedat.c", y)
 assert(y == x + (moe.noncelen + moe.maclen) * 2)
 fhi:close()
 fho:close()
---
-local fhi = io.open("moedat.c")
-local fho =  io.open("moedat.p2", "w")
+local fhi = io.open(fnc)
+local fho =  io.open(fnp2, "w")
 z, msg = moe.fhdecrypt(k, fhi, fho)
 assert(z and (z == x), msg)
 --~ print("moedat.p2", x, msg)
 fhi:close()
 fho:close()
-assert(he.fget("moedat.p2") == he.fget("moedat.p"))
-os.remove("moedat.p")
-os.remove("moedat.p2")
-os.remove("moedat.c")
+assert(he.fget(fnp2) == he.fget(fnp))
+os.remove(fnp)
+os.remove(fnp2)
+os.remove(fnc)
 print("test_moe:  ok")
