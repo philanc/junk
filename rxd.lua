4690f27beeb821c8da3cada4be19623a728ac917
philanc <philanc@gmail.com>
2019-12-04 21:23:35 -0500

    v0.8

--- rxd.lua
+++ rxd.lua
@@ -6,6 +6,11 @@
 === rxd
 
 
+
+v0.8
+	ban system removed
+	handler table replaced with one handler function
+
 ]]
 
 ------------------------------------------------------------------------
@@ -50,7 +55,6 @@ local rxcore = require "rxcore"
 -----------------------------------------------------------------------
 rxd = {  -- the rxd module
 	log = log,
-	handlers = {},  -- the request handler table
 	server = {},	-- the server object
 }
 
@@ -63,61 +67,20 @@ rxd = {  -- the rxd module
 
 
 -----------------------------------------------------------------------
--- server - ban system and anti-replay and other utilities
+-- server - anti-replay and other utilities
 
 
-function rxd.client_is_banned(server, client_ip)
-	local r
-	if server.whitelist[client_ip] then 
-		-- (whitelist overrides banned list)
-		return false
-	end
-	r = server.banned_ip_tbl[client_ip]
-	if not r then 
-		return false
-	end
-	server.banned_ip_tbl[client_ip] = r + 1
-	-- here we ignore the rollover risk. 
-	-- assume it is safe with int64.
-	if server.log_already_banned then 
-		log(strf("already banned ip %s, tries: %d", 
-		    client_ip, r))
-	end
-	return true
-end
 	
-function rxd.ban_counter_incr(server, client_ip)
-	local tries = he.incr(server.ban_tries, client_ip)
-	if tries > server.ban_max_tries then
-		he.incr(server.banned_ip_tbl, client_ip)
-		rxd.log("BANNED", client_ip)
-	end
-end
-
-function rxd.ban_counter_reset(server, client_ip)
-	-- clear the try-counter (after a valid request)
-	server.ban_tries[client_ip] = nil
-	-- auto whitelist
-	server.whitelist[client_ip] = true
-end
-
-function rxd.init_ban_list(server)
-	-- ATM, start with empty lists
-	server.ban_tries = {}
-	server.banned_ip_tbl = {}
-	server.whitelist = {}
-end
-	
-function rxd.init_used_reqid_list(server)
+function rxd.init_used_nonce_list(server)
 	-- ATM, start with empty list
-	server.reqid_tbl = {}
+	server.nonce_tbl = {}
 end
 	
-function rxd.is_reqid_used(server, reqid)
-	-- determine if reqid has recently been used
+function rxd.is_nonce_used(server, nonce)
+	-- determine if nonce has recently been used
 	-- then set it to used
-	local  r = server.reqid_tbl[reqid]
-	server.reqid_tbl[reqid] = true
+	local  r = server.nonce_tbl[nonce]
+	server.nonce_tbl[nonce] = true
 	return r
 end
 
@@ -160,17 +123,12 @@ local function handle_cmd(server, cmd, data)
 	return handler(ctx, cmd, data)
 end --handle_cmd
 
-function rxd.handle_req(reqid, code, arg, data)
+function rxd.handle_req(reqid, data)
 	-- return rcode, rarg, rdata
-	local rcode, rarg, rdata
-	local handler = rxd.handlers[code]
-	if handler then 
-		rcode, rarg, rdata = handler(reqid, code, arg, data)
-		rarg = rarg or 0
-		rcode = rcode or -2
-		return rcode, rarg, rdata
-	else
-		return -1, 0, nil
+	local rdata
+	
+	--XXXXX
+	
 	end
 end
 
@@ -191,71 +149,71 @@ local function serve_client(server, cso)
 	
 	local r, eno, msg
 	local nonce, ehdr, edata, hdr, data
-	local reqid, time
+	local nonce, time, rnd, ctr, nonces
 	local datalen, code, arg
 	local handler, rcode, rarg, rdata
 	
 	-- read nonce
 	nonce, eno = sock.read(cso, rxcore.NONCELEN)
 	if not nonce then msg = "reading nonce"; goto cerror end
-	reqid, time = rxcore.parse_nonce(nonce)
+	time, rnd, ctr = rxcore.parse_nonce(nonce)
+	nonces = rxcore.make_noncelist(time, rnd)
+	nonce = nonces[1] 	-- << this is on purpose:
+			-- to prevent an attacker to bypass anti-replay
+			-- with an initial nonce with ctr > 4
+			-- (keep using nonces[1] for anti-replay.)
 	
-	if rxd.is_reqid_used(server, reqid) then 
+	if rxd.is_nonce_used(server, nonce) then 
 		eno = -1
-		msg = "reqid reused"
-		rxd.ban_counter_incr(server, cip)
+		msg = "nonce reused"
+		goto cerror
 	end
 
 	if not rxd.is_time_valid(time) then
 		eno = -1
 		msg = "invalid time"
-		rxd.ban_counter_incr(server, cip)
+		goto cerror
 	end
 	
 	-- read req header
 	ehdr, eno = sock.read(cso, rxcore.HDRLEN)
 	if not ehdr then msg = "reading ehdr"; goto cerror end
 	
-	--unwrap req header (ctr=0)
-	datalen, code, arg = rxcore.unwrap_hdr(server.key, reqid, 0, ehdr)
+	--unwrap req header 
+	datalen = rxcore.unwrap_hdr(server.key, nonces[1], ehdr)
 	if not datalen then 
 		msg = "header decrypt error"
-		rxd.ban_counter_incr(server, cip)
 		eno = -1
 		goto cerror
 	else
 		-- header is valid (the header has been properly 
 		-- decrypted, so the client is assumed to be genuine.)
 		-- => reset the ban try counter for the client ip
-		rxd.ban_counter_reset(server, cip)
+		-- rxd.ban_counter_reset(server, cip)
 	end
 	
-	-- read data if needed
-	if datalen > 0 then 
-		edata, eno = sock.read(cso, datalen + rxcore.MACLEN)
-		if not edata then msg = "reading edata"; goto cerror end
-		-- unwrap data (ctr=1)
-		data, msg = unwrap_data(server.key, reqid, 1, edata)
-		if not data then
-			eno = -1
-			-- msg is set above
-			goto cerror
-		end
+	-- read data
+	edata, eno = sock.read(cso, datalen + rxcore.MACLEN)
+	if not edata then msg = "reading edata"; goto cerror end
+	-- unwrap data
+	data, msg = unwrap_data(server.key, nonces[2], edata)
+	if not data then
+		eno = -1
+		-- msg is set above
+		goto cerror
 	end
 	
 	-- handle the request
-	rcode, rarg, rdata = rxd.handle_req(reqid, code, arg, data)
+	rdata = rxd.handle_req(data)
 	
 	-- send the response
-	rhdr, rdata = rxcore.wrap_resp(server.key, reqid, rcode, rarg, rdata)
+	rhdr, rdata = rxcore.wrap_resp(server.key, nonces, rdata, rnd)
 	
 	r, eno = sock.write(cso, rhdr)
 	if not r then msg = "sending rhdr"; goto cerror end
 
-	if rdata then 
-		r, eno = sock.write(cso, rdata)
-		if not r then msg = "sending rdata"; goto cerror end
-	end
+	r, eno = sock.write(cso, rdata)
+	if not r then msg = "sending rdata"; goto cerror end
 	
 	do  -- this do block because return MUST be the last stmt of a block
 		return true 
