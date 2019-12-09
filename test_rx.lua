fd4065761ba6dd0eacee89258a12c5daa3cde005
philanc <philanc@gmail.com>
2019-12-09 18:44:31 -0500

    repl rx*.lua with rx.lua

--- test_rx.lua
+++ test_rx.lua
@@ -2,8 +2,7 @@
 
 server = require "rxconf"
 
-rxc = require "rxc"
-rxcore = require "rxcore"
+rx = require "rx"
 
 he = require 'he'  -- make he global for request chunks
 local hezen = require 'he.zen'
@@ -25,7 +24,7 @@ local function repr(x)
 end
 
 local function test_01()
-	print("req", rxc.request(server, "hello"))
+	print("req", rx.request(server, "hello"))
 end
 
 local function test_02()
@@ -37,9 +36,9 @@ local function test_02()
 		respt = {ok=true, errmsg="no error!!"}
 		return respt
 	]]
-	local rt, msg, ctx = rxc.request(server, dt)
+	local rt, msg, ctx = rx.request(server, dt)
 	if not rt then 
-		print("!! rxc.request error: ", msg, ctx)
+		print("!! rx.request error: ", msg, ctx)
 	else
 		rt = hunpack(rt)
 --~ 		he.pp(rt)
@@ -58,9 +57,9 @@ local function test_03()
 		pp(package.loaded)
 		return {ok=true}
 	]]
-	local rt, msg, ctx = rxc.request(server, dt)
+	local rt, msg, ctx = rx.request(server, dt)
 	if not rt then 
-		print("!! rxc.request error: ", msg, ctx)
+		print("!! rx.request error: ", msg, ctx)
 	else
 		rt = hunpack(rt)
 --~ 		he.pp(rt)
@@ -79,20 +78,20 @@ local function test_04()
 		print("--------------------")
 		return {ok=true}
 	]]
-	local rt, msg = rxc.lua(server, luacmd)
+	local rt, msg = rx.lua(server, luacmd)
 	print('msg:', msg)
 	pp(rt)
 end
 
 local function test_05()
 	print("--------------------test_05")
-	local r, msg = rxc.sh(server, "ls -l")
+	local r, msg = rx.sh(server, "ls -l")
 	print(r, msg)
 end
 
 local function test_05a()
 	print("--------------------test_05a")
-	local r, msg = rxc.sh(server, 'echo "popen process pid: $$" ')
+	local r, msg = rx.sh(server, 'echo "popen process pid: $$" ')
 	print(r, msg)
 end
 
@@ -105,7 +104,7 @@ local function test_06()
 		os.exit(ex)
  		--return {ok=true}
 	]]
-	local rt, msg = rxc.lua(server, luacmd)
+	local rt, msg = rx.lua(server, luacmd)
 	print(rt and rt.errmsg or msg)
 --~ 	pp(rt)
 end
@@ -123,24 +122,24 @@ local function test_times()
 		print("--------------------")
 		return rt
 	]]
-	local rt, msg = rxc.lua(server, luacmd)
+	local rt, msg = rx.lua(server, luacmd)
 	pf("client time: %d   server time: %d   client-server: %d",
 		rt.reqtime, rt.servtime, rt.reqtime - rt.servtime )
 end
 
 local function test_shutdown0()
 	print("--------------------shutdown requested!!")
-	pp(rxc.request(server, {exitcode=rxcore.SHUTDOWN} ))
+	pp(rx.request(server, {exitcode=rxcore.SHUTDOWN} ))
 end
 
 local function test_restart()
 	print("--------------------restart requested!!")
-	rxc.lua(server, "return {ok=true, exitcode=0}" )
+	rx.lua(server, "return {ok=true, exitcode=0}" )
 end
 
 local function test_shutdown()
 	print("--------------------shutdown requested!!")
-	rxc.lua(server, "return {ok=true, exitcode=1}" )
+	rx.lua(server, "return {ok=true, exitcode=1}" )
 end
 
 --~ test_01()
@@ -148,8 +147,8 @@ end
 --~ test_03()
 --~ test_04()
 --~ test_05()
---~ test_05a()
-test_06()
+test_05a()
+--~ test_06()
 --~ test_times()
 
 if arg[1] == "0" then test_restart() end
