9c20f180e5d08146e8fa27022d75f752a70ace1b
philanc <philanc@gmail.com>
2020-12-22 01:45:21 -0500

    adjusted to last he

--- citest.lua
+++ citest.lua
@@ -1,7 +1,7 @@
 
 he = require 'he'
 he.pmem()
-print(he.isodate())
+print(he.isodate(), he.isodate(nil, true)) -- local time, utc time
 exitcode = 0
 print("CI-Test. exit code:", exitcode)
 os.exit(exitcode)
